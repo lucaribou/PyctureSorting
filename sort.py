6190a3e02af4ada85ccb9965a86a9d7ae27bf0aa
Lucas <lucas.saissi@gmail.com>
2020-05-11 09:42:41 +0200

    adding log and stop moving duplicates

--- sort.py
+++ sort.py
@@ -1,50 +1,45 @@
-import os, fnmatch, re, shutil
+import os, fnmatch, re, shutil, logging
+from datetime import datetime
 from PIL import Image, ExifTags
 
-imagesBaseDirectory = "/sourcedir/"
+imagesBaseDirectory = "/volume1/photo/sort/"
+logging.basicConfig(filename=datetime.now().strftime("%m%d%Y_%H%M%S") + '.log',level=logging.DEBUG)
 
 for dirpath, dirnames, filenames in os.walk(imagesBaseDirectory):
-    for filename in [f for f in filenames if f.endswith('.jpg') or f.endswith('.png') or f.endswith('.JPG')]:
-        
-        imagePath = os.path.join(dirpath, filename)
-        year, month, day = None, None, None
+    if not '@eaDir' in dirpath:
+        for filename in [f for f in filenames if f.endswith('.jpg') or f.endswith('.png') or f.endswith('.JPG')]:
+            
+            imagePath = os.path.join(dirpath, filename)
+            year, month, day = None, None, None
 
-        if re.match("\d{4}-\d{2}-\d{2}.*", filename):
-            year = int(filename[:4])
-            month = int(filename[5:7])
-            day = int(filename[8:10])
+            if re.match("\d{4}-\d{2}-\d{2}.*", filename):
+                year = int(filename[:4])
+                month = int(filename[5:7])
+                day = int(filename[8:10])
 
-        else:
-            try:
-                image = Image.open(imagePath)
-                image_exif = image.getexif()
-                if image_exif and 36867 in image_exif:
-                    date_taken = image_exif[36867]
-                    year = int(date_taken[:4])
-                    month = int(date_taken[5:7])
-                    day = int(date_taken[8:10])
-            except IOError:
-                print("Can't open the file : " + imagePath)
-
-        if year is not None and month is not None and day is not None:
-            dirName = "/targetdir/" + str(year) + "/" + str(month).zfill(2) + "/" + str(day).zfill(2) + "/"
-            if not os.path.exists(dirName):
-                os.makedirs(dirName)
-                print("Directory " + dirName + " created")
-
-            new_image = dirName + filename
-            if not os.path.exists(new_image):
-                print("moving " + imagePath + " in " + new_image)
-                shutil.move(imagePath, new_image)
             else:
-                i = 1
-                base, extension = os.path.splitext(filename)
-                while True:
-                    new_name = os.path.join(dirName, base + "_" + str(i) + extension)
-                    if not os.path.exists(new_name):
-                        print("moving " + imagePath + " in " + new_name)
-                        shutil.move(imagePath, new_name)
-                        break 
-                    i += 1
-        else:
-            print("No date found for " + imagePath)
+                try:
+                    image = Image.open(imagePath)
+                    image_exif = image.getexif()
+                    if image_exif and 36867 in image_exif:
+                        date_taken = image_exif[36867]
+                        year = int(date_taken[:4])
+                        month = int(date_taken[5:7])
+                        day = int(date_taken[8:10])
+                except IOError:
+                    logging.warning("Can't open the file : " + imagePath)
+
+            if year is not None and month is not None and day is not None:
+                dirName = "/volume1/photo/" + str(year) + "/" + str(month).zfill(2) + "/" + str(day).zfill(2) + "/"
+                if not os.path.exists(dirName):
+                    os.makedirs(dirName)
+                    print("Directory " + dirName + " created")
+
+                new_image = dirName + filename
+                if not os.path.exists(new_image):
+                    print("moving " + imagePath + " in " + new_image)
+                    shutil.move(imagePath, new_image)
+                else:
+                    logging.info("Duplicate found : " + filename)
+            else:
+                logging.info("No date found for " + imagePath)
